6aaab846283c75cb82deabe550adf11733ef4cf0
waleedahmad98 <w.ahmad1024@gmail.com>
2022-05-16 02:30:58 +0500

    changes

--- src/components/AutomaticVerification.js
+++ src/components/AutomaticVerification.js
@@ -6,28 +6,31 @@ export default function AutomaticVerification(props) {
     const [toggler, setToggler] = useState("Verification Records")
     const [mode, setMode] = useState(0)
     const [modeText, setModeText] = useState("Pending Verifications")
-
+    const [display, setDisplay] = useState(0)
     return (
-        <div><div className='d-flex justify-content-between mt-5'>
-            <div>
-                <h4 style={{ fontWeight: "700" }}>{modeText}</h4>
-            </div>
-            <div>
-                <button className='anim-btn anim-btn-sm py-2' onClick={() => {
-                    if (mode === 0) {
-                        setToggler("Pending Verifications");
-                        setModeText("Verified Credentials")
-                        setMode(1)
-                    }
-                    else {
-                        setToggler("Verification Records");
-                        setModeText("Pending Verifications")
-                        setMode(0)
-                    }
+        <div>
+            <div className='d-flex justify-content-between mt-5'>
+                {display === 0 ? <><div>
+                    <h4 style={{ fontWeight: "700" }}>{modeText}</h4>
+                </div>
+                    <div>
+                        <button className='anim-btn anim-btn-sm py-2' onClick={() => {
+                            if (mode === 0) {
+                                setToggler("Pending Verifications");
+                                setModeText("Verified Credentials")
+                                setMode(1)
+                            }
+                            else {
+                                setToggler("Verification Records");
+                                setModeText("Pending Verifications")
+                                setMode(0)
+                            }
 
-                }}>{toggler}</button>
-            </div>
+                        }}>{toggler}</button>
+                    </div>
+                </> : <></>}
 
-        </div>{mode === 0 ? <UnverifiedCredentials address={props.address} /> : <></>}</div>
+            </div>{mode === 0 ? <UnverifiedCredentials address={props.address} setDisplay={setDisplay}/> : <></>}
+        </div>
     )
 }
--- src/components/UnverifiedCredentials.js
+++ src/components/UnverifiedCredentials.js
@@ -6,6 +6,7 @@ export default function UnverifiedCredentials(props) {
     const [uvc, setUvc] = useState([]);
     const [zeroCheck, setZeroCheck] = useState(false)
     const [veriferMode, setVerifierMode] = useState(0)
+    const [currvc, setCurrvc] = useState(null)
 
     useEffect(() => {
         console.log("add", props.address)
@@ -18,11 +19,14 @@ export default function UnverifiedCredentials(props) {
         })
     }, [])
 
-    const beginVerification = (txid) => {
-        axios.get(`https://stacks-node-api.testnet.stacks.co/extended/v1/tx/${txid}`).then(r => {
-            console.log(r.data)
+    const beginVerification = (vc) => {
+        axios.get(`https://stacks-node-api.testnet.stacks.co/extended/v1/tx/${vc.txid}`).then(r => {
+            setCurrvc(r.data)
+            setVerifierMode(1)
+            props.setDisplay(1)
+            console.log(r.data.contract_call.function_args[1].repr)
         })
-        setVerifierMode(1)
+
     }
 
     if (veriferMode === 0) {
@@ -37,8 +41,8 @@ export default function UnverifiedCredentials(props) {
                             <div className='d-flex flex-row'>
                                 <button className='card-btn me-3' onClick={() => { window.open(`https://explorer.stacks.co/txid/${vc.txid}?chain=${CHAIN_TYPE}`, "_blank") }}>DETAILS</button>
                                 <button className='card-btn me-3' onClick={() => {
-                                    
-                                    beginVerification(vc.txid);
+
+                                    beginVerification(vc);
                                 }}>VERIFY</button>
                             </div>
                         </div>
@@ -81,20 +85,23 @@ export default function UnverifiedCredentials(props) {
         return (
             <>
                 <div className='row vh-100 d-flex flex-wrap'>
-                    <div className='col-md-4'>
-                        {/* <iframe src={`${props.localProps.contract_call.function_args[3].repr.replace('"', "").replace('"', "")}`}
-                            style={{ border: "3px solid black" }} height="500" width="400" ></iframe> */}
+                    <div className='col-md-4 text-start'>
+                        <button className="anim-btn anim-btn-sm mb-3 mt-2" onClick={() => {
+                            props.setDisplay(0);
+                            setVerifierMode(0)
+                        }}>BACK</button>
+                        <iframe src={`${currvc.contract_call.function_args[3].repr.replace('"', "").replace('"', "")}`}
+                            style={{ border: "3px solid black" }} height="500" width="400" ></iframe>
                     </div>
-                    <div className='col-md-8 text-start'>
-                        {/* <div>
-                            <h5 class="mb-4" style={{ fontWeight: "700", fontSize: "4rem" }}>{props.localProps.contract_call !== undefined ? props.localProps.contract_call.function_args[2].repr.replace('"', "").replace('"', "") : <></>}</h5>
+                    <div className='col-md-8 text-start mt-5'>
+                        <div>
+                            <h5 class="mb-4" style={{ fontWeight: "700", fontSize: "4rem" }}>{currvc.contract_call !== undefined ? currvc.contract_call.function_args[2].repr.replace('"', "").replace('"', "") : <></>}</h5>
                             <div style={{ fontWeight: "700", fontSize: "2rem" }}>Owned By </div>
-                            <h6 class="mb-4" style={{ fontSize: "1.5rem" }}>{props.localProps.contract_call !== undefined ? <>{props.localProps.contract_call.function_args[0].repr}</> : <></>}</h6>
+                            <h6 class="mb-4" style={{ fontSize: "1.5rem" }}>{currvc.contract_call !== undefined ? <>{currvc.contract_call.function_args[0].repr}</> : <></>}</h6>
                             <div style={{ fontWeight: "700", fontSize: "2rem" }}>Issued By</div>
-                            <h6 class="mb-4" style={{ fontSize: "1.5rem" }} >{props.localProps.sender_address !== undefined ? <>{props.localProps.sender_address}</> : <></>}</h6>
-                            <div style={{ fontWeight: "700", fontSize: "2rem" }}>Date Issued</div>
-                            <div class="mb-4" style={{ fontSize: "1.5rem" }}>{props.localProps.burn_block_time_iso !== undefined ? <>{getDateTime(props.localProps.burn_block_time_iso)}</> : <></>}</div>
-                        </div> */}
+                            <h6 class="mb-4" style={{ fontSize: "1.5rem" }} >{currvc.sender_address !== undefined ? <>{currvc.sender_address}</> : <></>}</h6>
+                            <button className='card-btn me-3 py-3' onClick={() => { window.open(`https://explorer.stacks.co/txid/${currvc.tx_id}?chain=${CHAIN_TYPE}`, "_blank") }}>DETAILS</button>
+                        </div>
 
                     </div>
                 </div>
